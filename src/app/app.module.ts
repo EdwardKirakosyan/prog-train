d4b9b7a000d7fca20b43a3f1ec2bc4bd9d4ce7bd
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-16 09:21:22 +0400

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from "@angular/common";
+import { DemoComponent } from './src/app/demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
