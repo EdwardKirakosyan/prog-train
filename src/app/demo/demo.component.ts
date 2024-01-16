5afc5aecf9cf1551e8b0653975c5de8f26b6d2ea
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-16 11:33:14 +0400

    c

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,10 +1,40 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  SimpleChanges,
+  OnChanges,
+  DoCheck, AfterContentInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrl: './demo.component.css'
 })
-export class DemoComponent {
+export class DemoComponent implements OnInit, OnChanges, DoCheck, AfterContentInit {
   @Input() value: string = 'wsap'
+
+  constructor() {
+    console.log('con called')
+    console.log(this.value)
+  }
+
+  ngOnChanges(change: SimpleChanges) {
+    console.log('ssssssss')
+    console.log(change)
+  }
+
+  ngOnInit() {
+    console.log('ngcaled')
+    console.log(this.value)
+  }
+
+  ngDoCheck() {
+    console.log('sssDo')
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit')
+  }
 }
